57c4c120e9d9b721d57606f5d21f28fbe124f9a8
DmitryK <infosecoff@yandex.ru>
2023-02-08 23:56:59 +0300

    #1

---
+++ pepvpn/pepvpn/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pepvpn project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pepvpn.settings')
+
+application = get_asgi_application()
